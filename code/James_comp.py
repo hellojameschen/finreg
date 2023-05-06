8e3522831dddd787223ddc35f351e9e0ece86c33
James Chen <hellojameschen@gmail.com>
2023-05-06 16:27:04 -0400

    get best

--- code/James_comp.py
+++ code/James_comp.py
@@ -5,11 +5,10 @@ BASE_DIR = "/Users/jameschen/Team Name Dropbox/James Chen/FINREGRULEMAKE2/finreg
 
 df = pd.read_csv(BASE_DIR + "test2_df.csv")
 
-score_cols = list(filter(lambda x: "score" in x,df.columns))
 
-for score_col in score_cols:
-    threshold_fail = df[score_col]<0.95
-    df.loc[threshold_fail, score_col] = np.NaN
-    df.loc[threshold_fail, df.columns[df.columns.get_loc(score_col)-1]] = np.NaN
-
-df['best_match_score'] = df[score_cols].max(axis=1)
+def get_best(df):
+    score_cols = list(filter(lambda x: "score" in x,df.columns))
+    df['best_match_score'] = df[score_cols].max(axis=1)
+    df['best_match_name'] = df[score_cols].idxmax(axis=1).str.split(':').str[0] + ':best_match_name'
+    df['best_match_name'] = df.apply(lambda row: row.loc[row['best_match_name']] if row['best_match_name']==row['best_match_name'] else None, axis=1)
+    return df[['best_match_score', 'best_match_name']]

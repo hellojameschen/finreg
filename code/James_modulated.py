18d253ed53c306602ce64368d47df0544be629a3
James Chen <hellojameschen@gmail.com>
2023-06-29 17:52:22 -0400

    yay

--- code/James_modulated.py
+++ code/James_modulated.py
@@ -609,51 +609,67 @@ def clean_covariate_df(covariate_df):
 
     df = covariate_df
     df = df[list(filter(lambda x: not "submitter" in x,df.columns))]
-    # df = df[df['comment_org_name']!='']
-    # df.to_csv(BASE_DIR + "data/match_data/match_all_covariates_df_" + curr_date + ".csv")
+    df = df[df['comment_org_name']!='']
+    df.to_csv(BASE_DIR + "data/match_data/match_all_covariates_df_" + curr_date + ".csv")
 
-    # df = pd.read_csv(BASE_DIR + "data/match_data/match_all_covariates_df_" + curr_date + ".csv")
-    # df = df.drop("Unnamed: 0", axis=1)
+    df = pd.read_csv(BASE_DIR + "data/match_data/match_all_covariates_df_" + curr_date + ".csv")
+    df = df.drop("Unnamed: 0", axis=1)
 
-    # threshold = 0.95
+    threshold = 0.95
 
-    # score_cols = list(filter(lambda x: "score" in x,df.columns))
-    # for score_col in score_cols:
-    #     threshold_fail = df[score_col]<threshold
-    #     all_cols = list(filter(lambda x: score_col.split(':')[0] in x,df.columns))
-    #     df.loc[threshold_fail, all_cols] = np.NaN
+    score_cols = list(filter(lambda x: "score" in x,df.columns))
+    for score_col in score_cols:
+        threshold_fail = df[score_col]<threshold
+        all_cols = list(filter(lambda x: score_col.split(':')[0] in x,df.columns))
+        df.loc[threshold_fail, all_cols] = np.NaN
 
 
-    # name_cols = list(filter(lambda x: "best_match_name" in x,df.columns))
-    # exact_matches = pd.DataFrame()
-    # for name_col in name_cols:
-    #     exact_matches[name_col] = df[name_col]==df['comment_org_name']
+    name_cols = list(filter(lambda x: "best_match_name" in x,df.columns))
+    exact_matches = pd.DataFrame()
+    for name_col in name_cols:
+        exact_matches[name_col] = df[name_col]==df['comment_org_name']
 
-    # new_col = (exact_matches.sum(axis=1)>0).astype(int)
-    # df.insert(loc = 5,
-    #       column = 'exact_match_present',
-    #       value = new_col)
+    new_col = (exact_matches.sum(axis=1)>0).astype(int)
+    df.insert(loc = 5,
+          column = 'exact_match_present',
+          value = new_col)
     
-    # cols = list(df.columns)
-    # front = [
-    #     'comment_agency',
-    #     'original_org_name',
-    #     'comment_url',
-    #     'docket_id',
-    #     'comment_org_name',
-    #     'num_org_matches',
-    #     'exact_match_present',
-    #     ]
-    # cols[:len(front)]= front
+    cols = list(df.columns)
+    front = [
+        'comment_agency',
+        'original_org_name',
+        'comment_url',
+        'docket_id',
+        'comment_org_name',
+        'num_org_matches',
+        'exact_match_present',
+        ]
+    cols[:len(front)]= front
     
-    # df = df[cols]
+    df = df[cols]
 
-    # df.to_csv(BASE_DIR + "data/match_data/match_df_" + curr_date + ".csv")
     return df
 
+def get_best(df):
+    score_cols = list(filter(lambda x: "score" in x,df.columns))
+    df['best_match_score'] = df[score_cols].max(axis=1)
+    df['dataset'] = df[score_cols].idxmax(axis=1).str.split(':').str[0]
+    df['cleaned_best_match_name'] = df.apply(lambda row: row.loc[row['dataset'] + ':best_match_name'] if row['dataset']==row['dataset'] else None, axis=1)
+    df['original_best_match_name'] = df.apply(lambda row: row.loc[row['dataset'] + ':original_match_name'] if row['dataset']==row['dataset'] else None, axis=1)
 
+    return df[['original_org_name', 'best_match_score', 'comment_org_name', 'cleaned_best_match_name', 'original_best_match_name', 'dataset']]
+
+def get_validation(df):
+    temp = get_best(df).groupby('original_org_name')
+    df = temp.first()
+    df['frequency']=temp.count()['best_match_score']
+    df = df.reset_index()
+    df = df.sort_values(by=['frequency','best_match_score'], ascending=False)
+    df = df[['frequency', 'best_match_score', 'original_org_name', 'comment_org_name', 'cleaned_best_match_name', 'original_best_match_name', 'dataset']]
+    df['hand_match']=''
+    return df
        
 org_name_df = get_organization_dataset()
-key_names_df = get_comment_dataset().iloc[:1000,:]
+key_names_df = get_comment_dataset().iloc[:,:]
 covariate_df = get_matches(org_name_df,key_names_df)
-df = clean_covariate_df(covariate_df)
+df = get_validation(covariate_df)
